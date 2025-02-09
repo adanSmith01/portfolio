031ce2bec7ad47792819d50d35e8507db3f2bb66
Adan Smith <110949361+adanSmith01@users.noreply.github.com>
2025-02-09 07:20:56 +0000

    Delete the 'DetailOrQuote' attribute

--- src/BlazorApp/Models/AboutMe.cs
+++ src/BlazorApp/Models/AboutMe.cs
@@ -4,5 +4,4 @@ public class AboutMe
 {
     public string Description { get; set; } = string.Empty;
     public List<string> Skills { get; set; } = new();
-    public string DetailOrQuote { get; set; } = string.Empty;
 }
