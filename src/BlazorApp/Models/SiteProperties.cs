eed0c6a40f6601af1a9b43309cb5e0f887ca54b1
Adan Smith <110949361+adanSmith01@users.noreply.github.com>
2025-02-11 23:11:42 +0000

    Add an object of type 'Image' for the 'Home' component

--- src/BlazorApp/Models/SiteProperties.cs
+++ src/BlazorApp/Models/SiteProperties.cs
@@ -5,7 +5,7 @@ public class SiteProperties
     public string Name { get; set; } = string.Empty;
     public string Title { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
-    public string DevDotTo { get; set; } = string.Empty;
+    public Image? Profile { get; set; }
     public string GitHub { get; set; } = string.Empty;
     public string Instagram { get; set; } = string.Empty;
     public string LinkedIn { get; set; } = string.Empty;
